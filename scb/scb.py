523d878a0f159de2ebdb2daf9b8730a97971dbd9
Ruben Selander <77073569+Reversehobo@users.noreply.github.com>
2024-11-22 19:43:17 +0100

    Add jstat_converter.py for JSON-stat data conversion and manipulation

--- scb/scb.py
+++ scb/scb.py
@@ -8,9 +8,9 @@ import math
 
 SESSION = requests.Session()
 BASE_URL = "https://api.scb.se/ov0104/v2beta/api/v2"
-DEFAULT_LANG = "sv"  # Default language currently "sv" and "en" is supported
+DEFAULT_LANG = None
 DEFAULT_FORMAT = "csv2"  # Default format for data response
-CONFIG = None
+config = None
 PAGE_SIZE = 10000
 
 
@@ -193,58 +193,105 @@ def _get_queries(variables: dict, limit: int) -> list[dict]:
     return [_construct_query(config) for config in request_configs]
 
 
+def set_base_url(url: str) -> None:
+    """Set the base URL for the module to use."""
+    global BASE_URL
+    BASE_URL = url
+
+
+def set_default_lang(lang: str) -> None:
+    """Set the default language for the module to use."""
+    global DEFAULT_LANG
+    valid_langs = _get_valid_languages()
+    if lang not in valid_langs:
+        raise ValueError(f"Invalid language: {lang}. Valid languages are {valid_langs}")
+    DEFAULT_LANG = lang
+
+
+# {"languages": [{"id": "sv", "label": "Svenska"}, {"id": "en", "label": "English"}]}
+
+
+def _get_valid_languages() -> list[str]:
+    """Get the valid languages for the API."""
+    config = get_config()
+    langs = config["languages"]
+    return [lang["id"] for lang in langs]
+
+
+def _get_valid_formats() -> list[str]:
+    """Get the valid data formats for the API."""
+    config = get_config()
+    return config["dataFormats"]
+
+
+def _get_language_param(lang: str = None) -> dict:
+    """Determine the language parameter for API calls."""
+    global DEFAULT_LANG
+
+    valid_langs = _get_valid_languages()
+    if lang is not None:
+        if lang not in valid_langs:
+            raise ValueError(
+                f"Invalid language: {lang}. Valid languages are {valid_langs}"
+            )
+        return {"lang": lang}
+
+    if DEFAULT_LANG is not None:
+        return {"lang": DEFAULT_LANG}
+
+    return {}  # No language parameter
+
+
 def get_config() -> dict:
     """Get the configuration and set it as a global variable."""
-    global CONFIG
-    if CONFIG is None:
+    global config
+    if config is None:
         response = SESSION.get(BASE_URL + "/config")
-        CONFIG = response.json()
-    return CONFIG
+        config = response.json()
+    return config
 
 
-def get_folder(folder_id: str = "", lang: str = DEFAULT_LANG) -> dict:
+def get_folder(folder_id: str = "", lang: str = None) -> dict:
     """Get the folder information for a specific folder ID."""
-    response = SESSION.get(f"{BASE_URL}/navigation/{folder_id}", params={"lang": lang})
+    params = _get_language_param(lang)
+    response = SESSION.get(f"{BASE_URL}/navigation/{folder_id}", params=params)
     return response.json()
 
 
 def get_tables(
-    lang: str = DEFAULT_LANG,
     query: str = None,
     past_days: int = None,
     include_discontinued: bool = True,
+    lang: str = None,
 ) -> dict:
     """
     Fetch available tables with various filter options.
 
     Args:
-        lang: The language for the response (default is 'en').
         query: Search criteria to filter tables by name or other attributes.
         past_days: Filters tables that were updated in the last N days.
         include_discontinued: Whether to include discontinued tables (default is False).
+        lang: The language for the response. Optional. Overrides the default language if set.
 
     Returns:
         A dictionary of available tables matching the search criteria.
     """
     url = f"{BASE_URL}/tables"
     params = {
-        "lang": lang,
+        **_get_language_param(lang),
         "query": query,
         "pastDays": past_days,
-        "includeDiscontinued": str(
-            include_discontinued
-        ).lower(),  # API expects a string 'true' or 'false'
+        "includeDiscontinued": str(include_discontinued).lower(),
         "pageNumber": 1,
         "pageSize": PAGE_SIZE,
     }
 
-    # Remove None values from the params dictionary
     params = {k: v for k, v in params.items() if v is not None}
 
     response = SESSION.get(url, params=params)
     response_dict = response.json()
     tables = response_dict["tables"]
-    # remove the links and type element from each table, also remove description if empty string
+
     for table in tables:
         table.pop("links", None)
         table.pop("type", None)
@@ -254,50 +301,74 @@ def get_tables(
     return tables
 
 
-def get_metadata(table_id: str, lang: str = DEFAULT_LANG) -> dict:
+def get_metadata(table_id: str, as_json_stat: bool = True, lang: str = None) -> dict:
     """
     Fetch metadata for a specific table.
 
     Args:
         table_id: The ID of the table to fetch metadata for.
-        lang: The language for the response. Optional.
+        as_json_stat: Whether to return the metadata in JSON-Stat 2 format. Optional.
+        lang: The language for the response. Optional. Overrides the default language if set.
     """
     url = f"{BASE_URL}/tables/{table_id}/metadata"
-    params = {"lang": lang}
+    params = _get_language_param(lang)
+    if as_json_stat:
+        params["outputFormat"] = "json-stat2"
+
     response = SESSION.get(url, params=params)
     return response.json()
 
 
-def get_variables(table_id: str, lang: str = DEFAULT_LANG) -> dict:
+def get_variables(table_id: str, lang: str = None) -> dict:
     """
     Fetch the variables for a specific table.
 
     Args:
         table_id: The table ID to fetch variables for.
-        lang: The language for the response (default is 'en').
+        lang: The language for the response. Optional. Overrides the default language if set.
 
     Returns:
         A dictionary of variables for the specified table.
     """
-    metadata = get_metadata(table_id, lang)
+    metadata = get_metadata(table_id, as_json_stat=False, lang=lang)
     return metadata["variables"]
 
 
-def get_data(
-    table_id: str, lang: str = DEFAULT_LANG, format: str = DEFAULT_FORMAT, **kwargs
-):
+def get_codelist(codelist_id: str, lang: str = None) -> dict:
+    """
+    Fetch a codelist by its ID.
+
+    Args:
+        codelist_id: The ID of the codelist to fetch.
+        lang: The language for the response. Optional. Overrides the default language if set.
+
+    Returns:
+        A dictionary of the codelist.
+    """
+    url = f"{BASE_URL}/codeLists/{codelist_id}"
+    params = _get_language_param(lang)
+    response = SESSION.get(url, params=params)
+    return response.json()
+
+
+def get_data(table_id: str, format: str = "csv2", lang: str = None, **kwargs):
     """
     Fetch data for a specific table.
 
     Args:
         table_id: The ID of the table to fetch data from.
         lang: The language for the response (default is 'en').
-        format: The format for the data response (default is 'json').
+        format: The format for the data response (default is 'csv2').
+        lang: The language for the response. Optional. Overrides the default language if set.
         kwargs: The query to filter the data.
 
     Returns:
         A dictionary of data for the specified table.
     """
+    valid_formats = _get_valid_formats()
+    if format not in valid_formats:
+        raise ValueError(f"Invalid format: {format}. Valid formats are {valid_formats}")
+
     metadata = get_metadata(table_id, lang)
     variables = metadata["variables"]
     query = {"selection": []}
@@ -321,7 +392,7 @@ def get_data(
                 )
 
     url = f"{BASE_URL}/tables/{table_id}/data"
-    params = {"lang": lang, "outputFormat": format}
+    params = {**_get_language_param(lang), "outputFormat": format}
     response = SESSION.post(url, params=params, json=query)
 
     if "json" in response.headers["Content-Type"]:
@@ -330,19 +401,28 @@ def get_data(
         return response.text
 
 
-def get_all_data(table_id: str, lang: str = DEFAULT_LANG, file_name: str = None):
+def get_all_data(
+    table_id: str,
+    format: str = "csv2",
+    lang: str = None,
+):
     """
     Fetch all data for a specific table.
 
     Args:
         table_id: The ID of the table to fetch data from.
-        lang: The language for the response (default is 'sv').
-        file_name: The name of the file to save the data to (optional).
+        format: The format for the data response (default is 'csv2').
+        lang: The language for the response. Optional. Overrides the default language if set.
     """
 
+    config = get_config()
+    valid_formats = config["dataFormats"]
+    if format not in valid_formats:
+        raise ValueError(f"Invalid format: {format}. Valid formats are {valid_formats}")
+
     variables = get_variables(table_id, lang)
-    limit = CONFIG["maxDataCells"]
-    min_request_interval = CONFIG["timeWindow"] / CONFIG["maxCallsPerTimeWindow"] + 0.1
+    limit = config["maxDataCells"]
+    min_request_interval = config["timeWindow"] / config["maxCallsPerTimeWindow"] + 0.1
     last_request_time = time.time()
 
     simple_variables = {}
@@ -368,7 +448,7 @@ def get_all_data(table_id: str, lang: str = DEFAULT_LANG, file_name: str = None)
         response = SESSION.post(url, params=params, json=query)
         data.append(response.text)
 
-    if file_name:
-        _save_csv(_combine_csv_strings(data), file_name)
-
-    return _combine_csv_strings(data)
+    if "csv" in format:
+        return _combine_csv_strings(data)
+    else:
+        return data
